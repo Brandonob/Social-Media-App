09fba5e31d3a45150757b1c3a4e20c67412fe4dc
BrandonBrown <brandonbrown@pursuit.org>
2020-05-22 04:56:03 -0400

    connect routers to app and tested

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,18 @@ const app = express();
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const PORT = 3000;
+const usersRouter = require('./db/routes/users/users')
+const postsRouter = require('./db/routes/posts/posts')
+const likesRouter = require('./db/routes/likes/likes')
+const commentsRouter = require('./db/routes/comments/comments')
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use("/users", usersRouter)
+app.use("/posts", postsRouter)
+app.use("/likes", likesRouter)
+app.use("/comments", commentsRouter)
 
 app.listen(PORT, () => console.log("listening"));
--- backend/db/routes/users/users.js
+++ backend/db/routes/users/users.js
@@ -1,3 +1,7 @@
 const users = require('express').Router();
 
+users.get("/", (req, res) => {
+    res.json("This is working")
+})
+
 module.exports = users;
