5d1cf210b6828f9634a190aa93502420ac447e17
BrandonBrown <brandonbrown@pursuit.org>
2020-05-25 21:06:47 -0400

    complete login navbar

---
+++ frontend/facebookapp/src/features/loginPage/LoginNavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { NavLink } from 'react-router-dom'
+
+const loginNavBar = () => {
+    return (
+        <div>
+            <h1>facebook</h1>
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button>login</button>
+            </form>  
+        </div>
+    )
+}
+
+export default loginNavBar
